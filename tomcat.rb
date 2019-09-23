e36d1ff0db754f12bbe4511aa6a25825248b5608
Nickolay Polyakov <nickolay.pol@gmail.com>
2019-09-23 18:39:59 +0300

    fix url

---
+++ tomcat.rb
@@ -0,0 +1,62 @@
+class TomcatAT8 < Formula
+  desc "Implementation of Java Servlet and JavaServer Pages"
+  homepage "https://tomcat.apache.org/"
+  url "http://archive.apache.org/dist/tomcat/tomcat-8/v8.5.45/bin/apache-tomcat-8.5.45.tar.gz"
+  sha256 "d9b58d12979243fba01bbbbb33c140c8593940c005ec9acef1b2f54ce9b3d0fc"
+
+  bottle :unneeded
+
+  keg_only :versioned_formula
+
+  depends_on :java => "1.7+"
+
+  def install
+    # Remove Windows scripts
+    rm_rf Dir["bin/*.bat"]
+
+    # Install files
+    prefix.install %w[NOTICE LICENSE RELEASE-NOTES RUNNING.txt]
+    libexec.install Dir["*"]
+    bin.install_symlink "#{libexec}/bin/catalina.sh" => "catalina"
+  end
+
+  plist_options :manual => "catalina run"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Disabled</key>
+        <false/>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/catalina</string>
+          <string>run</string>
+        </array>
+        <key>KeepAlive</key>
+        <true/>
+      </dict>
+    </plist>
+  EOS
+  end
+
+  test do
+    ENV["CATALINA_BASE"] = testpath
+    cp_r Dir["#{libexec}/*"], testpath
+    rm Dir["#{libexec}/logs/*"]
+
+    pid = fork do
+      exec bin/"catalina", "start"
+    end
+    sleep 3
+    begin
+      system bin/"catalina", "stop"
+    ensure
+      Process.wait pid
+    end
+    assert_predicate testpath/"logs/catalina.out", :exist?
+  end
+end
